a07d94ab184dc68f8663b69b0b42da756446e791
warrenm2 <35501013+explodinghat@users.noreply.github.com>
2023-03-31 09:12:09 +0100

    basic cluster

--- main.tf
+++ main.tf
@@ -1,27 +1,25 @@
-terraform {
-  required_version = ">= 1.3"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
 }
 
-# Reference to the current subscription.  Used when creating role assignments
-data "azurerm_subscription" "current" {}
-
-resource "random_pet" "rg" {
-  length = 1
-  prefix = var.name
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-k8s-resources"
+  location = var.location
 }
 
-# The main resource group for this deployment
-resource "azurerm_resource_group" "default" {
-  name     = "${random_pet.rg.id}-${var.environment}-rg"
-  location = var.location
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,40 +1,7 @@
-// Naming
-variable "name" {
-  type        = string
-  description = "Location of the azure resource group."
-  default     = "demo-tfquickstart"
+variable "prefix" {
+  description = "A prefix used for all resources in this example"
 }
-
-variable "environment" {
-  type        = string
-  description = "Name of the deployment environment"
-  default     = "dev"
-}
-
-// Resource information
 
 variable "location" {
-  type        = string
-  description = "Location of the azure resource group."
-  default     = "UKWest"
-}
-
-// Node type information
-
-variable "node_count" {
-  type        = number
-  description = "The number of K8S nodes to provision."
-  default     = 1
-}
-
-variable "node_type" {
-  type        = string
-  description = "The size of each node."
-  default     = "Standard_D2s_v3"
-}
-
-variable "dns_prefix" {
-  type        = string
-  description = "DNS Prefix"
-  default     = "tfq"
+  description = "The Azure Region in which all resources in this example should be provisioned"
 }
